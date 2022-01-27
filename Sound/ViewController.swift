f5e77b661a813063d817c421d30cf880caf0284d
RyunosukeKurokawa <kuroryu.basket@gmail.com>
2022-01-27 16:01:33 +0900

    Initial Commit

---
+++ Sound/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sound
+//
+//  Created by 黒川龍之介 on 2022/01/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
